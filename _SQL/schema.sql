854dbab067dd6d535a5a1b39ad41d7c42718d9bf
Raphael Darley <raphael@raphaeldarley.com>
2022-09-06 22:40:40 +0000

    add tag junction table

--- _SQL/schema.sql
+++ _SQL/schema.sql
@@ -1,20 +1,26 @@
- CREATE TABLE `notes` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `text` TEXT NOT NULL,
-  `category_id` INT,
-  `source` VARCHAR(1023),
-  `tag_ids` VARCHAR(1023),
-  `timestamp` timestamp NOT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE `notes` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `text` TEXT NOT NULL,
+    `category_id` INT,
+    `source` VARCHAR(1023),
+    `timestamp` timestamp NOT NULL,
+    PRIMARY KEY (`id`)
+);
 
+CREATE TABLE `categories` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
 
- CREATE TABLE `categories` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE `tags` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+);
 
-
- CREATE TABLE `tags` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`id`));
+CREATE TABLE `tag_junction` (
+    `note_id` INT NOT NULL,
+    `tag_id` INT NOT NULL,
+    PRIMARY KEY (`note_id`, `tag_id`)
+);
