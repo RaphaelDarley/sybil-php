d4a6311f9278be8088b12512ce849f69f049d618
Raphael Darley <raphael@raphaeldarley.com>
2022-09-07 07:56:25 +0000

    add display with tags

--- display.php
+++ display.php
@@ -5,8 +5,9 @@ include_once 'utils.php';
 include_once 'database_init.php';
 
 $get_stmt = $pdo->prepare("
-    SELECT *
+    SELECT notes.id as note_id, text, source, timestamp, categories.name as category
     FROM notes
+    INNER JOIN categories ON notes.category_id = categories.id
     LIMIT 100
     ");
 $get_stmt->execute();
@@ -29,6 +30,11 @@ $rows = $get_stmt->fetchAll();
 </head>
 
 <body>
+
+    <a href="draft.php">
+        <strong>add notes</strong>
+    </a>
+
     <?php
     $result_num = count($rows);
     echo "$result_num results found";
@@ -45,8 +51,40 @@ $rows = $get_stmt->fetchAll();
             <th>tags</th>
         </tr>
 
-        <?php foreach ($rows as $db_row) {
-            $id = $db_row["id"];
+        <?php
+
+        function get_tags($pdo, $note_id)
+        {
+            // $tag_stmt = $pdo->prepare("
+            // SELECT tags.name as tag
+            // FROM notes
+            // INNER JOIN tag_junction
+            //     ON notes.id = tag_junction.note_id
+            // INNER JOIN tag_junction
+            //     ON tag_junction.tag_id = tags.id
+            // ");
+            $tag_stmt = $pdo->prepare("
+            SELECT tags.name as tag
+            FROM tag_junction
+            INNER JOIN tags
+                ON tag_junction.tag_id = tags.id
+            WHERE note_id = :note_id
+            ");
+            $tag_stmt->execute(["note_id" => $note_id]);
+            $rows = $tag_stmt->fetchAll();
+            // var_dump($rows);
+            $tags = array_map(function ($t) {
+                return $t["tag"];
+            }, $rows);
+            return join(", ", $tags);
+            // return $tags;
+        }
+
+
+
+        foreach ($rows as $db_row) {
+
+            $id = $db_row["note_id"];
 
             $timestamp = $db_row["timestamp"];
 
@@ -56,10 +94,14 @@ $rows = $get_stmt->fetchAll();
 
             $category = $db_row["category"];
 
-            $tags = $db_row["tags"];
-            if ($tags !== NULL) {
-                $tags = join(", ", json_decode($tags));
-            }
+            // $tags = $db_row["tags"];
+            // if ($tags !== NULL) {
+            //     $tags = join(", ", json_decode($tags));
+            // }
+
+            $tags = get_tags($pdo, $id);
+
+            // var_dump($tags);
 
             $html_row = "<tr> <td>$id</td> <td>$timestamp</td> <td>$text</td> <td>$source</td> <td>$category</td> <td>$tags</td> </tr>";
 
